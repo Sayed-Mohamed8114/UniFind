97decb9cb144b73cb5a73d0c9f031197b56a62d8
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-04-24 11:49:26 +0300

    edit flyon bug 1

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,6 +1,15 @@
-const validRoutes = ["/", "/index.html"];
+const validRoutes = [
+  "/",
+  "/index.html",
+  "/pages/admin.html",
+  "/pages/register.html",
+  "/pages/signIn.html",
+  "/pages/user.html",
+  "/pages/errorPage.html",
+];
 
-if (!validRoutes.includes(window.location.pathname)) {
-  window.location.href = "/pages/errorPage.html";
+const currentPath = window.location.pathname;
+
+if (!validRoutes.includes(currentPath)) {
+  window.location.replace("/pages/errorPage.html");
 }
-
